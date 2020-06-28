70c1726adec03a0f5aa2d933c99951db41cd4b00
yutaokamoto <yutaokamoto1997@gmail.com>
2020-06-28 17:26:26 +0900

    new!

--- practice/a/main.cpp
+++ practice/a/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    
+    int a;
+    cin >> a;
+    int b,c;
+    cin >> b >> c;
+    string s;
+    cin >> s;
+    cout << a+b+c << s << endl;
     return 0;
 }
