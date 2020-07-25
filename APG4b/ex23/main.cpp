19baa3edd521baaefa1e893fc32bfccf3b5d3369
yutaokamoto <yutaokamoto1997@gmail.com>
2020-07-25 20:46:15 +0900

    complete!

--- APG4b/ex23/main.cpp
+++ APG4b/ex23/main.cpp
@@ -1,2 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int N;
+vector<int> A;
+
+int main(){
+    cin >> N;
+    A.resize(N);
+    map<int, int> count;
+    pair<int, int> ans = make_pair(0, 0);
+    for(int i = 0; i < N; i++){
+        cin >> A.at(i);
+        count[A.at(i)]++;
+        /*if(count.count(i)){
+            count[i]++;
+        }
+        else{
+            count[i] = 1;
+        }*/
+    }
+
+    for(auto p: count){
+        //cout << p.first << " " << p.second << endl;
+        if(ans.second < p.second){
+            ans = p;
+        }
+    }
+    cout << ans.first << " " << ans.second << endl;
+    return 0;
+}
