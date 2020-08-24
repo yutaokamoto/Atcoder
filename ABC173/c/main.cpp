7953d616630fe0d787456e0b5f4f3d9b1f10d2a4
yutaokamoto <yutaokamoto1997@gmail.com>
2020-08-24 17:01:50 +0900

    new!

--- ABC173/c/main.cpp
+++ ABC173/c/main.cpp
@@ -11,21 +11,25 @@ int main(){
     for(int i = 0; i < H; i++){
         cin >> cell[i];
     }
-    for_each(cell.begin(), cell.end(), [](string x){cout << x << endl;});
+    //for_each(cell.begin(), cell.end(), [](string x){cout << x << endl;});
 
-    /*int cnt = 0;
+    int ans = 0;
     for(int r = 0; r < pow(2, H); r++){
         for(int c = 0; c < pow(2, W); c++){
+            int cnt = 0;
             for(int i = 0; i < H; i++){
                 for(int j = 0; j < W; j++){
-                    if(((r>>i)&1==0) && ((c>>i)&1==0) && cell[i][j]=='#'){
+                    if( (((r>>i)&1)==0) && (((c>>j)&1)==0) && cell[i][j]=='#' ){
                         cnt++;
                     }
                 }
             }
+            if(cnt == K){
+                ans++;
+            }
         }
     }
 
-    cout << cnt << endl;*/
+    cout << ans << endl;
     return 0;
 }
