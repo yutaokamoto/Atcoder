54948239df7e6972e084a47c3b3752107154efc4
yutaokamoto <yutaokamoto1997@gmail.com>
2021-07-10 01:01:48 +0900

    new!

---
+++ abc208/c/ex.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+using ll = long long;
+#define rep(i, n) for(int i=0; i<(n); i++)
+
+int main(){
+    cout << 1000000000000/7 << endl;
+    cout << 1000000000000%7 << endl;
+    
+	return 0;
+}
