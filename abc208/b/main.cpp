a7e0c77be34502932ca0ab710ce3fc8377db41e8
yutaokamoto <yutaokamoto1997@gmail.com>
2021-07-09 23:29:38 +0900

    completed

--- abc208/b/main.cpp
+++ abc208/b/main.cpp
@@ -5,9 +5,21 @@ using namespace std;
 using ll = long long;
 #define rep(i, n) for(int i=0; i<n; i++)
 
+/*int fractorial(int n){
+	if(n==1)return 1;
+	return n*fractorial(n-1);
+}*/
+int fractorial(int n){return n ? n*fractorial(n-1) : 1;}
+
 int main(){
 	int p;
 	cin >> p;
-	
+	int ans = 0;
+	for(int i=10; i>=1; i--){
+		int yen = fractorial(i);
+		ans += p/yen;
+		p = p%yen;
+	}
+	cout << ans << endl;
 	return 0;
 }
